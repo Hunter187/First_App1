5e0976416c677db962ea10a33fbc6de135cee13c
Hunter187 <pszain4@gmail.com>
2019-02-07 10:04:26 +0200

    add news-single

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -1,4 +1,6 @@
+import { NewsService } from './../news.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-news',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.page.scss'],
 })
 export class NewsPage implements OnInit {
-
-  constructor() { }
+    data :any;
+  constructor(private NewsService:NewsService, private router:Router) { }
 
   ngOnInit() {
+    this.NewsService.getData('top-headlines?sources=al-jazeera-english').subscribe(data => {
+      console.log(data);
+      this.data =data;
+    })
+    
+  }
+  onGoToNewsSinglePage(article){
+      this.NewsService.currentArticle=article;
+      this.router.navigate(['/news-single']);
   }
 
 }
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,3 +1,4 @@
+import { NewsPageModule } from './../news/news.module';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
@@ -10,7 +11,9 @@ import { Tab1Page } from './tab1.page';
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{ path: '', component: Tab1Page }]),
+    NewsPageModule
+    
   ],
   declarations: [Tab1Page]
 })
